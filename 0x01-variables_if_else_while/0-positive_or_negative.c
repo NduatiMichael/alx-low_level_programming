fd71f5b18e00cfdd2074fc55586004a1447a1790
NduatiMichael <mknduati@gmail.com>
2022-07-01 17:21:07 -0700

    Positive or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,11 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/*
+ * betty style doc for function main goes there 
+ */
 int main(void)
 {
-/* mani function*/
 	int n;
 
 	srand(time(0));
