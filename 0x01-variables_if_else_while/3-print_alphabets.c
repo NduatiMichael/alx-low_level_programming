c573856471abefde53df9c08f85fbe6735ad4ac0
NduatiMichael <mknduati@gmail.com>
2022-07-01 19:34:11 -0700

    alphaBET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main - the entire alphabet                                                    * Return: the alphabet in small letters                                         */                                                                            
+ * main - the entire alphabet
+ * Return: the alphabet in small letters                                         */                                                                            
 int main(void)
 {
 	char i;
 	char t;
-
-       	for (i = 'a'; i <= 'z'; i++)
+	
+	for (i = 'a'; i <= 'z'; i++)
 		putchar(i);
-       	for (t = 'A'; t <= 'Z'; t++)
+	for (t = 'A'; t <= 'Z'; t++)
 		putchar(t);
 	putchar('\n');
 	return (0);
