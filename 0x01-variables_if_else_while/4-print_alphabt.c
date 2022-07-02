1ac43ededa7cfd43d2fe845e42f58b1a7ab9cac2
NduatiMichael <mknduati@gmail.com>
2022-07-01 19:57:39 -0700

    alphabet 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - the entire alphabet
+ * Return: the alphabet in small letters
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i== 'q' || i== 'e')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
