e2ac91f23f46f2bb92ee1399fb946d855bbaae6d
NduatiMichael <mknduati@gmail.com>
2022-07-07 01:35:09 +0300

    times table

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,18 @@
  */
 int print_last_digit(int n)
 {
-	int lastDigit;
+	int a;
 
 	if (n < 0)
-	{
-	n = -n;
-	}
-	lastDigit = n % 10;
-	_putchar(lastDigit + '0');
-	return (lastDigit);
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
 #endif
