9100d545ca655a728dcdd81a9d73bdb5e00569d5
NduatiMichael <mknduati@gmail.com>
2022-07-06 23:46:50 +0300

    check is a character is an alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+/**
+ *_isalpha - checks if character is an alphabet
+ *
+ *@c: the character to check
+ * Return: 1 if lowercase 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
+#endif
