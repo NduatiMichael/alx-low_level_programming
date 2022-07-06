22f65efcdee0cfc16ad42a167f77ef2c31d9c6dd
NduatiMichael <mknduati@gmail.com>
2022-07-06 20:50:40 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+i#include "main.h"
+/**
+ *Description: prints _putchar
+ *
+ * Return: Always 0
+ */
+int _putchar(char);
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
