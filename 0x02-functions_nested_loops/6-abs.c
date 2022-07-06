0ae6c9834e14488c19fa842a90853f2986740c32
NduatiMichael <mknduati@gmail.com>
2022-07-07 00:47:51 +0300

    print absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include "main.h"
+/**
+ *_abs - Prints the absolute value of an input
+ *
+ *@int: the number to check
+ * Return: absolute
+ */
+int _abs(int);
+{
+	int absolute;
+
+	if (int > 0)
+	{
+		absolute = int;
+	}
+	else if (n == 0)
+	{
+		absolute = 0;
+	}
+	else
+	{
+		absolute = int * -1;
+	}
+	return (absolute);
+}
+#endif
