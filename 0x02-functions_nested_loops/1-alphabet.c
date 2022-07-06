7395d46d99557c47c03a762e37231eedbc6e6ef6
NduatiMichael <mknduati@gmail.com>
2022-07-06 22:10:23 +0300

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,7 +8,7 @@ void print_alphabet(void)
 {
         char alphabet = 'a';
 
-        for (alphabet = 'a'; alphabet =< 'z'; alphabet++)
+        for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		int_ putchar(alphabet);
         putchar('\n');
         return ;
