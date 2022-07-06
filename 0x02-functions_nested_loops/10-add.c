210350769e924a5a7befdbdeab825d0754abea55
NduatiMichael <mknduati@gmail.com>
2022-07-07 01:44:13 +0300

    function to find the sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *add - find the sum of two numbers
+ *@a: first number
+ *@b: Second number
+ *Return: the sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
