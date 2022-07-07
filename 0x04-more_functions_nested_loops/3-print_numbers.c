51f92a2001ddb85811500fd0b738daf0f0259b8c
NduatiMichael <mknduati@gmail.com>
2022-07-07 23:07:51 +0300

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * print_numbers - function to print numbers
- * * Return: no return value
+ * Return: no return value
  */
 void print_numbers(void)
 {
 	int counter = 0;
+
+	int _putchar(char);
 
 	while (counter <= 9)
 	{
